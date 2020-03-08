52b6594061dd64f697bcd8f0e2f7819a6fa5bbc8
Jacek Szydlowski <szydlowski.jk@gmail.com>
2020-03-08 21:38:18 +0100

    require path update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const ex = express()
 const port = process.env.PORT
 
-const ue = require('enigma')
+const ue = require('./enigma.js')
 
 ex.get('/', (req, res) => {
     res.send('Hello Enigma')
