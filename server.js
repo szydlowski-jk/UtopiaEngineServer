fbf364302c7c0eac080143dad952dc123df91edc
Jacek Szydlowski <szydlowski.jk@gmail.com>
2020-03-13 08:33:19 +0100

    idgenerator added

--- server.js
+++ server.js
@@ -79,3 +79,17 @@ function handleApiCall ( body ) {
 
     return result
 }
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function generateGameId () {
+    return makeid(6)
+}
