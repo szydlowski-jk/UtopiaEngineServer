77d3a4ae6654a0f9cadd762f56cd499c2fc2bab3
Jacek Szydlowski <szydlowski.jk@gmail.com>
2020-03-14 15:46:17 +0100

    server refactor start

--- server.js
+++ server.js
@@ -1,199 +1,202 @@
 const express = require('express')
 const cors = require('cors')
-const ex = express()
+const app = express()
 const port = process.env.PORT || 3000
+const engine = require('./engine.js')
 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', express.sendFile('page/index.html'));
+app.get('/g/*', express.sendFile('page/game.html'));
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://server:serverpassword@utopiadb-qtk8d.mongodb.net/test?retryWrites=true&w=majority";
-let dbcoll;
-
-ex.use(cors())
-ex.use(express.json())
-
-const uejs = require('./engine.js')
-let ue = new uejs.UtopiaEngine()
-
-DBConnect()
-
-ex.use('/', express.static('page'))
-
-ex.post('/game/:gameId/:action', (req, res) => {
-    if ( req.body != null ) {
-        // TODO HERE
-    }
-    res.send(req.params)
-})
-
-ex.get('/test/new', (req, res) => {
-    console.log('new game')
-    let id = newGame()
-    ue.data.gameid = id
-    res.send( ue.data )
-})
-
-ex.get('/test/get/:id', (req, res) => {
-    console.log('get game' + req.params)
-    DBget( req.params['id'] )
-    res.send( ue.data )
-})
-
-ex.get('/test/day/:id', (req, res) => {
-    console.log('get game' + req.params)
-    DBget( req.params['id'] )
-    ue.doAction('doRest', {})
-    DBset( req.params['id'], ue.data )
-    res.send( ue.data )
-})
+//let dbcoll;
 
 
-// ex.get('/', (req, res) => {
-//     res.send('Utopia Engine')
+//let ue = new uejs.UtopiaEngine()
+
+//DBConnect()
+
+
+// ex.post('/game/:gameId/:action', (req, res) => {
+//     if ( req.body != null ) {
+//         // TODO HERE
+//     }
+//     res.send(req.params)
 // })
 
-// https://utopiaengine.herokuapp.com/3sv5jd/action
+// ex.get('/test/new', (req, res) => {
+//     console.log('new game')
+//     let id = newGame()
+//     let ue = new uejs.UtopiaEngine()
+//     ue.data.gameid = id
+//     res.send( ue.data )
+// })
 
-ex.get('/ue', (req, res) => {
-    res.send(JSON.stringify(ue.data))
-})
+// ex.get('/test/get/:id', (req, res) => {
+//     console.log('get game' + req.params)
+//     DBget( req.params['id'] )
+//     res.send( ue.data )
+// })
 
-
-ex.post('/API', (req, res) => {
-    res.send( handleApiCall( req.body ) )
-    // console.log(req.body)
-    // res.send(req.body)
-})
-
-ex.get('/API', (req, res) => {
-    console.log(req.body)
-    res.send(ue.getActions())
-})
-
-ex.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
-
-
-// function connectDB () {
-//     client.connect(err => {
-//         const dbcol = client.db("UtopiaEngineDB").collection("DataSheets")
-//         let ued = new UtopiaData()
+// ex.get('/test/day/:id', (req, res) => {
+//     console.log('get game' + req.params)
+//     DBget( req.params['id'] )
+//     ue.doAction('doRest', {})
+//     DBset( req.params['id'], ue.data )
+//     res.send( ue.data )
+// })
 
 
+// // ex.get('/', (req, res) => {
+// //     res.send('Utopia Engine')
+// // })
 
-//        const collection = client.db("test").collection("devices");
-//        console.log(collection);
-        // perform actions on the collection object
-    //     client.close();
-    // });
+// // https://utopiaengine.herokuapp.com/3sv5jd/action
 
-//}
-
-
-function iterateFunc(doc) {
-    console.log(JSON.stringify(doc, null, 4));
- }
-
-function errorFunc(error) {
-    console.log(error);
-}
+// ex.get('/ue', (req, res) => {
+//     res.send(JSON.stringify(ue.data))
+// })
 
 
-async function DBConnect () {
-    console.log('DB connect')
-    try {
-        const client = new MongoClient(uri, { useNewUrlParser: true, reconnectInterval: 1000, reconnectTries: Number.MAX_VALUE });
-        await client.connect()
-        dbcoll = client.db("UtopiaEngineDB").collection("DataSheets")
-    } catch ( err ) {
-        dbcoll = null
-    }
+// ex.post('/API', (req, res) => {
+//     res.send( handleApiCall( req.body ) )
+//     // console.log(req.body)
+//     // res.send(req.body)
+// })
 
-}
+// ex.get('/API', (req, res) => {
+//     console.log(req.body)
+//     res.send(ue.getActions())
+// })
 
-// function DBDisconnect () {
-//     client.close
+// ex.listen(port, () => {
+//     console.log(`listening on port ${port}`)
+// })
+
+
+// // function connectDB () {
+// //     client.connect(err => {
+// //         const dbcol = client.db("UtopiaEngineDB").collection("DataSheets")
+// //         let ued = new UtopiaData()
+
+
+
+// //        const collection = client.db("test").collection("devices");
+// //        console.log(collection);
+//         // perform actions on the collection object
+//     //     client.close();
+//     // });
+
+// //}
+
+
+// function iterateFunc(doc) {
+//     console.log(JSON.stringify(doc, null, 4));
+//  }
+
+// function errorFunc(error) {
+//     console.log(error);
 // }
 
-async function DBget ( id ) {
-    console.log('DBget ' + id);
 
-    if ( dbcoll ) {
-        try {
-            let cursor = await dbcoll.findOne({gameId: id})
-            ue.data = cursor.data
-            console.log(ue.data)
-        } catch ( err ) {
-            console.log( 'DBget error: ' + err )
-        }
-    }
-}
+// async function DBConnect () {
+//     console.log('DB connect')
+//     try {
+//         const client = new MongoClient(uri, { useNewUrlParser: true, reconnectInterval: 1000, reconnectTries: Number.MAX_VALUE });
+//         await client.connect()
+//         dbcoll = client.db("UtopiaEngineDB").collection("DataSheets")
+//     } catch ( err ) {
+//         dbcoll = null
+//     }
 
-function DBset ( id, data ) {
-    if ( dbcoll ) {
-        try {
-            dbcoll.updateOne( { gameId: id }, { $set: { data: ue.data } })
-        } catch ( err ) {
-            console.log( err )
-        }
-    }
-}
+// }
+
+// // function DBDisconnect () {
+// //     client.close
+// // }
+
+// async function DBget ( id ) {
+//     console.log('DBget ' + id);
+
+//     if ( dbcoll ) {
+//         try {
+//             let cursor = await dbcoll.findOne({gameId: id})
+//             ue.data = cursor.data
+//             console.log(ue.data)
+//         } catch ( err ) {
+//             console.log( 'DBget error: ' + err )
+//         }
+//     }
+// }
+
+// function DBset ( id, data ) {
+//     if ( dbcoll ) {
+//         try {
+//             dbcoll.updateOne( { gameId: id }, { $set: { data: ue.data } })
+//         } catch ( err ) {
+//             console.log( err )
+//         }
+//     }
+// }
 
 
-function newGame () {
-    let data = {}
-    data.gameId = generateGameId()
-    data.data = new uejs.UtopiaData()
-    data.history = []
+// function newGame () {
+//     let data = {}
+//     data.gameId = generateGameId()
+//     data.data = new uejs.UtopiaData()
+//     data.history = []
 
-    if ( dbcoll ) {
-        try {
-            dbcoll.insertOne( data )
-        } catch ( err ) {
-            console.log( err )
-        }
-    }
+//     if ( dbcoll ) {
+//         try {
+//             dbcoll.insertOne( data )
+//         } catch ( err ) {
+//             console.log( err )
+//         }
+//     }
 
-    return data.gameId
-}
+//     return data.gameId
+// }
 
-function validateBody ( body ) {
-    if ( body.action != null &&
-         body.gameId != null &&
-         body.params != null ) {
-             return true
-    }
+// function validateBody ( body ) {
+//     if ( body.action != null &&
+//          body.gameId != null &&
+//          body.params != null ) {
+//              return true
+//     }
 
-    return false
-}
+//     return false
+// }
 
-function getDBdata ( gameId ) {
-    // todo
-}
+// function getDBdata ( gameId ) {
+//     // todo
+// }
 
-function handleApiCall ( body ) {
-    if ( body.action == "getState" ) {
-        return {result: true, state: ue.data, req: body}
-    }
+// function handleApiCall ( body ) {
+//     if ( body.action == "getState" ) {
+//         return {result: true, state: ue.data, req: body}
+//     }
 
-    let result = ue.doAction( body.action, body.params )
-    result.data = ue.data
-    result.req = body
+//     let result = ue.doAction( body.action, body.params )
+//     result.data = ue.data
+//     result.req = body
 
-    return result
-}
+//     return result
+// }
 
-/* https://stackoverflow.com/a/1349426 */
-function makeid(length) {
-    var result           = '';
-    var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
+// /* https://stackoverflow.com/a/1349426 */
+// function makeid(length) {
+//     var result           = '';
+//     var characters       = 'abcdefghijklmnopqrstuvwxyz0123456789';
+//     var charactersLength = characters.length;
+//     for ( var i = 0; i < length; i++ ) {
+//        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     }
+//     return result;
+// }
 
-function generateGameId () {
-    return makeid(6)
-}
+// function generateGameId () {
+//     return makeid(6)
+// }
