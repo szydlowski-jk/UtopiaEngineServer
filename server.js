74670a79d21934fcaa0613e0a8cfa177a87cc237
Jacek Szydlowski <szydlowski.jk@gmail.com>
2020-03-15 19:21:24 +0100

    next

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ app.get('/g/:gameid(\\w{6})/data', ( req, res ) => {
     mdb.Get( gameid, function(back) {
         console.log('THAT Callback: ', back);
         result = back;
-        res.json( { result: true, data: out, res: result } );
+//        res.json( { result: true, data: out, res: result } );
     } );
+    res.json( {dupa: "dupa"})
 });
 
 
